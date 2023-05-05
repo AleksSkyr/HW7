f9d846a3e30411dd65aeeb0bf78086bdab02ac56
AleksSkyr <greshnik1499@gmail.com>
2023-05-05 20:52:49 +0300

    7

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,10 @@ public class Main {
         tasc1();
         tasc2();
         tasc3();
+        tasc4();
+        tasc5();
+        tasc6();
+        tasc7();
 
     }
 
@@ -37,26 +41,70 @@ public class Main {
         System.out.println("Задача 3");
         int population = 12000000;
         int mortality = 8;
-        int fertility = 17;
+        int growth = 17;
         int y = 0;
-        int fertilityY = fertility - mortality / 1000;
-        population = population + fertilityY ;
-        while (y < 10){
+        int growthY = (population - growth) / 1000;
+        int mortalityY = (population * mortality) / 1000;
+        while (y < 10) {
             y = y + 1;
-            System.out.println("Через " + y +" лет, численность населения составляет " + population);
+            population = population + growthY - mortalityY;
+            System.out.println("Через " + y + " лет, численность населения составляет " + population);
 
-    }
-//        ;
-//
         }
-//        Рассчитайте, какая численность населения будет через 10 лет
-//        int salary = 65535;
-//        int total = 0;
-//        while (total < 1_000_000) {
-//            total = total + total/100;
-//            total = total + salary;
-//            System.out.println(total);
     }
 
+    public static void tasc4() {
+        System.out.println("Задача 4");
+        int salary = 15000;
+        int total = 0;
+        int i = 0;
+        while (total <= 12_000_000) {
+            total = total + total * 7 / 100;
+            i++;
+            total = total + salary;
+            System.out.println(i + " месяц " + total + " сумм накоплений");
+        }
+    }
 
+    public static void tasc5() {
+        System.out.println("Задача 5");
+        int salary = 15000;
+        int total = 0;
+        int i = 0;
+        while (total <= 12_000_000) {
+            total = total + total * 7 / 100;
+            i++;
+            total = total + salary;
+            if (i % 6 == 0) {
+                System.out.println(i + " месяц " + total + " сумм накоплений");
+            }
+        }
+    }
 
+    public static void tasc6() {
+        System.out.println("Задача 6");
+        int salary = 15000;
+        int total = 0;
+        int month = 0;
+        while (month <= 108) {
+            total = total + total * 7 / 100;
+            month++;
+            total = total + salary;
+            if (month % 6 == 0) {
+                System.out.println(month + " месяц " + total + " сумм накоплений");
+            }
+        }
+    }
+
+    public static void tasc7() {
+        System.out.println("Задача 7");
+        int oneFraidey = 2;
+        int dey = 0;
+        for (dey = 0; dey < 31; dey++) {
+            if (dey == oneFraidey) {
+                oneFraidey = oneFraidey + 7;
+                System.out.println("Сегодня пятница, " + dey + "-е число. Необходимо подготовить отчет");
+            }
+        }
+    }
+}
